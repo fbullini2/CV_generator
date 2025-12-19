e96af633d9dde5c52198f37d155660e6b24a50e2
fbullini2 <fbullini@gmail.com>
2025-12-19 01:15:58 +0100

    added isManagementRelated field in Achievement

--- src/main/java/ai/agenticity/cv/generator/domain/Achievement.java
+++ src/main/java/ai/agenticity/cv/generator/domain/Achievement.java
@@ -12,64 +12,71 @@ package ai.agenticity.cv.generator.domain;
  * in the table film_movietitle.
  */
 public class Achievement implements Comparable<Achievement> {
-    
-    /** The title of the movie. */
+
+    private final boolean isManagementRelated;
+    /**
+     * The title of the movie.
+     */
     protected String ach_desc;
     protected String role;
-    /** The year the movie was released. */
+    /**
+     * The year the movie was released.
+     */
     protected int year;
-    /** The duration of the movie in minutes. */
+    /**
+     * The duration of the movie in minutes.
+     */
     protected int duration;
-    /** The list of directors. */
-   
-    public Achievement(String ach_desc,int duration){
-    this.ach_desc=ach_desc;
-    this.duration=duration;
+
+    public Achievement(String ach_desc, boolean isManagementRelated) {
+        this.ach_desc = ach_desc;
+        this.isManagementRelated=isManagementRelated;
     }
-    public Achievement(String ach_desc){
-    this.ach_desc=ach_desc;
-   
-    }
-    
+
     /**
      * Returns the title in the correct form.
+     *
      * @return a title
      */
     public String ach_desc() {
         return ach_desc;
-    }  
+    }
+
     /**
      * @return the originalTitle
      */
     public String getRole() {
         return role;
     }
+
     /**
      * @return the year
      */
     public int getYear() {
         return year;
     }
+
     /**
      * @param year the year to set
      */
     public void setYear(int year) {
         this.year = year;
     }
+
     /**
      * @return the duration
      */
     public int getDuration() {
         return duration;
     }
-    
+
     @Override
     public String toString() {
         return ach_desc;
     }
-    
+
     public int compareTo(Achievement o) {
-		return ach_desc.compareTo(o.ach_desc);
-	}
-    
+        return ach_desc.compareTo(o.ach_desc);
+    }
+
 }
